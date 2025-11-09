c5010038f273dcddd047bbbf5a475cfdd37aa2ed
prajumna <prajumnaalasyam999@gmail.com>
2025-11-08 23:26:10 -0600

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,13 +1,1 @@
-function addToCart(productId) {
-    // Find product
-    let product = null;
-    for (const category in products) {
-        product = products[category].find(p => p.id === productId);
-        if (product) break;
-    }
-    
-    if (product) {
-        cart.push(product);
-        addMessage(`Added ${product.name} to cart! 🛒`);
-    }
-}
+
