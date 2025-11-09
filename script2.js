09bc31e7344399a0c412740684b1c3f5b6fd9e02
prajumna <prajumnaalasyam999@gmail.com>
2025-11-08 23:17:11 -0600

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,14 +1,1 @@
-function searchProducts(query) {
-    const results = [];
-    
-    // Search all categories
-    for (const category in products) {
-        products[category].forEach(product => {
-            if (product.name.toLowerCase().includes(query.toLowerCase())) {
-                results.push(product);
-            }
-        });
-    }
-    
-    return results;
-}
+
